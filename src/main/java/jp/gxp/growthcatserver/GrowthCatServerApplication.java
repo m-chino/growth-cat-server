5a64a25d270d396902c30118ef5f7ea7059ca4c5
Mamichi Chino <m.chino@gxp.co.jp>
2019-03-22 17:59:11 +0900

    プロジェクトの雛形を作成

---
+++ src/main/java/jp/gxp/growthcatserver/GrowthCatServerApplication.java
@@ -0,0 +1,13 @@
+package jp.gxp.growthcatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthCatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowthCatServerApplication.class, args);
+    }
+
+}
